bb28fbff5732b617eb8feae177e01b90144ece91
Senpoo Senpoosen <EriaSenpoo@hotmail.com>
2020-02-17 08:06:27 +0100

    init

---
+++ H2_jQuery_Lommeregner/jQuery_calculator.js
@@ -0,0 +1,15 @@
+﻿function number(num) {
+    $('.history').val($('history').val() + num);
+}
+
+function operator(symbol) {
+    $(".history").val($(".history").val() + symbol);
+}
+
+function equals() {
+    $('.history').val(eval($('.history').val())); 
+}
+
+function clear_everything() {
+    $('.history').val('')
+}
